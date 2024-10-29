cd72216f8eeaa18a2d87f92afeee664c215b5366
daxpetersen <dax.petersen@ucas-edu.net>
2024-10-28 21:41:13 -0600

    Update Dessert_Shop.py

--- Dessert_Shop.py
+++ Dessert_Shop.py
@@ -1,25 +1,41 @@
-value = 0
-x = 0
-class dessert():
-    def __init__(self,name,amount):
+# dessert.py
+
+class DessertItem:
+    def __init__(self, name: str):
         self.name = name
-        self.amount = amount
-    def __str__(self):
-        return f"{self.amount * self.value}"
-class candy(dessert):
-    value += 2
-    x = 1
-class cookie(dessert):
-    value += 4
-    x = 2
-class sundae(dessert):
-    value += 6
-    x = 3
-class icecream(dessert):
-    value += 8
-    x = 4
-class sprinkles(icecream):
-    value += 8.25
-    x = 5
-print(candy("dax",1))
-    
+
+class Candy(DessertItem):
+    def __init__(self, name: str, candy_weight: float, price_per_pound: float):
+        super().__init__(name)
+        self.candy_weight = candy_weight
+        self.price_per_pound = price_per_pound
+
+    def calculate_cost(self):
+        return self.candy_weight * self.price_per_pound
+
+class Cookie(DessertItem):
+    def __init__(self, name: str, cookie_quantity: int, price_per_dozen: float):
+        super().__init__(name)
+        self.cookie_quantity = cookie_quantity
+        self.price_per_dozen = price_per_dozen
+
+    def calculate_cost(self):
+        return (self.cookie_quantity / 12) * self.price_per_dozen
+
+class IceCream(DessertItem):
+    def __init__(self, name: str, scoop_count: int, price_per_scoop: float):
+        super().__init__(name)
+        self.scoop_count = scoop_count
+        self.price_per_scoop = price_per_scoop
+
+    def calculate_cost(self):
+        return self.scoop_count * self.price_per_scoop
+
+class Sundae(IceCream):
+    def __init__(self, name: str, scoop_count: int, price_per_scoop: float, topping_name: str, topping_price: float):
+        super().__init__(name, scoop_count, price_per_scoop)
+        self.topping_name = topping_name
+        self.topping_price = topping_price
+
+    def calculate_cost(self):
+        return super().calculate_cost() + self.topping_price
